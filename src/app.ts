741cfaee44af516824470d37df8ca6340d79f3e2
Faez Catrina <faezc@hotmail.com>
2021-12-16 17:41:13 -0700

    Update to class example

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  employees: string[] = [];
   
   constructor(n: string) {
     this.name = n;
@@ -8,8 +9,20 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 
 accounting.describe;
