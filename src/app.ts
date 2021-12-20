4dabae5374b9c71de15b4fc8c5d80fd6e979afc9
Faez Catrina <faezc@hotmail.com>
2021-12-19 18:22:21 -0700

    Initial intersection types example

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,17 @@
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-  (a: number, b: number): number;
-}
-
-let add: AddFn;
-
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+interface Admin {
+  name: string,
+  privileges: string[],
 };
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
-}
+interface Employee {
+  name: string;
+  startDate: Date;
+};
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
+interface ElevatedEmployee extends Employee, Admin {}
 
-class Person implements Greetable {
-  name?: string;
-  age = 30;
-
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
-  }
-
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + " " + this.name);
-    } else {
-      console.log("Hi!");
-    }
-  }
-}
-
-let user1: Greetable;
-
-user1 = new Person("Max");
-
-user1.greet("Hi there - I am");
-
-console.log(user1);
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
