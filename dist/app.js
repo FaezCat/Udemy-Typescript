5f2b7a0591a82034f748465b3a71f1530641844d
Faez Catrina <faezc@hotmail.com>
2021-12-19 18:49:05 -0700

    Type guards expanded example

--- dist/app.js
+++ dist/app.js
@@ -20,3 +20,26 @@ function printEmployeeInformation(emp) {
     }
 }
 printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo ..." + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if ("loadCargo" in vehicle) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
