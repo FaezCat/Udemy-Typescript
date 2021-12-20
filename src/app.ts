84cd66f52bacb698e0f04fad7bc870c95655090c
Faez Catrina <faezc@hotmail.com>
2021-12-19 18:43:10 -0700

    Type guards examples

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,42 @@
-interface Admin {
+type Admin = {
   name: string,
   privileges: string[],
 };
 
-interface Employee {
+type Employee = {
   name: string;
   startDate: Date;
 };
 
-interface ElevatedEmployee extends Employee, Admin {}
+type ElevatedEmployee = Employee & Admin;
 
 const e1: ElevatedEmployee = {
   name: "Max",
   privileges: ["create-server"],
   startDate: new Date(),
 };
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
