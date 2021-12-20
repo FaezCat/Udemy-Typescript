1c97becdc220aff5ba1050ffc3c0e175d41ac83f
Faez Catrina <faezc@hotmail.com>
2021-12-19 17:45:02 -0700

    Interfaces example

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("New section!");
+let user1;
+user1 = {
+    name: "Faez",
+    age: 30,
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    }
+};
+user1.greet("Hi there - I am");
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("New section!");
+interface Person {
+  name: string;
+  age: number;
+  greet(phrase: string): void;
+}
+
+let user1: Person;
+
+user1 = {
+  name: "Faez",
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+user1.greet("Hi there - I am");
