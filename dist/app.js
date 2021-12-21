9e244ef18b752c5d9a2bcff11418bcd615b48041
Faez Catrina <faezc@hotmail.com>
2021-12-20 17:11:29 -0700

    Discriminated union type example

--- dist/app.js
+++ dist/app.js
@@ -37,9 +37,21 @@ const v1 = new Car();
 const v2 = new Truck();
 function useVehicle(vehicle) {
     vehicle.drive();
-    if ("loadCargo" in vehicle) {
+    if (vehicle instanceof Truck) {
         vehicle.loadCargo(1000);
     }
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
