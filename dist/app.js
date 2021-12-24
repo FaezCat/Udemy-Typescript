41fbb125f0865458c2862f9961993e879d91ad4f
Faez Catrina <faezc@hotmail.com>
2021-12-23 18:51:32 -0700

    Generic types example using built in generics

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("hi!");
+const names = []; // string[]
+// names[0].split(" ");
+// returns a promise of the generic type string - typecasted using the <> operators
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 2000);
+});
+promise.then(data => {
+    // data.split(" ");
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hi!");
+const names: Array<string> = []; // string[]
+// names[0].split(" ");
+
+
+// returns a promise of the generic type string - typecasted using the <> operators
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
+
+promise.then(data => {
+  // data.split(" ");
+});
