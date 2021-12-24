25e68abe234c132fdd3a179335c036a43f59b68e
Faez Catrina <faezc@hotmail.com>
2021-12-23 19:08:27 -0700

    Generic function example using T and U generic types

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,18 @@
 "use strict";
-const names = []; // string[]
-// names[0].split(" ");
-// returns a promise of the generic type string - typecasted using the <> operators
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(10);
-    }, 2000);
-});
-promise.then(data => {
-    // data.split(" ");
-});
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
+// // returns a promise of the generic type string - typecasted using the <> operators
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+// promise.then(data => {
+//   // data.split(" ");
+// });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+console.log(merge({ name: "Max" }, { age: 30 }));
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj);
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
-const names: Array<string> = []; // string[]
-// names[0].split(" ");
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
 
 
-// returns a promise of the generic type string - typecasted using the <> operators
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// // returns a promise of the generic type string - typecasted using the <> operators
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then(data => {
-  // data.split(" ");
-});
+// promise.then(data => {
+//   // data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+console.log(merge({ name: "Max" }, { age: 30 }));
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+
+console.log(mergedObj);
