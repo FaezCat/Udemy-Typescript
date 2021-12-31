fbe6a5cc7df421cc201c87e3ced4c8902fc55396
Faez Catrina <faezc@hotmail.com>
2021-12-31 13:38:01 -0500

    Multiple decorator example - multiple decorators are executed bottom to top

--- dist/app.js
+++ dist/app.js
@@ -6,13 +6,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 function Logger(logString) {
+    console.log("LOGGER FACTORY");
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
 function WithTemplate(template, hookId) {
+    console.log("TEMPLATE FACTORY");
     return function (constructor) {
+        console.log("Rendering template...");
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
         if (hookEl) {
@@ -21,7 +24,7 @@ function WithTemplate(template, hookId) {
         }
     };
 }
-// @Logger("LOGGING - PERSON")
+// executes second
 let Person = class Person {
     constructor() {
         this.name = "Max";
@@ -29,6 +32,9 @@ let Person = class Person {
     }
 };
 Person = __decorate([
+    Logger("LOGGING")
+    // executes first
+    ,
     WithTemplate('<h1>My Person Object</h1>', 'app')
 ], Person);
 const pers = new Person();
