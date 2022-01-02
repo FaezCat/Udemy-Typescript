8cb2a29291a1f04c122ab68d53a158c4c6b52bea
Faez Catrina <faezc@hotmail.com>
2022-01-02 18:08:03 -0500

    Class property decorators example

--- dist/app.js
+++ dist/app.js
@@ -39,3 +39,27 @@ Person = __decorate([
 ], Person);
 const pers = new Person();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log("Property decorator!");
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this.price = val;
+        }
+        else {
+            throw new Error("Invalid price - should be a positive value!");
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
